c06b42d30446a0b9a27a7be0560877dff14f24bc
Hiram Méndez <hirammendez000@gmail.com>
2025-03-13 10:08:45 -0600

    deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+PROJECT_ID="modular-glider-449518-b9"
+REGION="us-central1"
+SERVICE_NAME="twitter-publisher"
+
+# Set the Google Cloud project
+gcloud config set project $PROJECT_ID
+
+# Deploy the application to Google Cloud Run
+gcloud run deploy $SERVICE_NAME \
+  --source . \
+  --project $PROJECT_ID \
+  --region $REGION \
+  --allow-unauthenticated \
+  --memory "8Gi" \
+  --cpu "8" 
+
+# Check deployment status
+if [ $? -eq 0 ]; then
+  echo "Deployment successful!"
+else
+  echo "Deployment failed."
+  exit 1
+fi
